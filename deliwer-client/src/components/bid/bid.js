a5009f88ba16a19f763597b5c6e9c85d34eca7fb
Vedant <vmahes4@uic.edu>
2022-04-09 22:56:37 -0500

    Added Fonts,Router Setup & created initial components

---
+++ deliwer-client/src/components/bid/bid.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bid = () => {
+  return <div>bid</div>;
+};
+
+export default Bid;
---
+++ deliwer-client/src/components/drivers/drivers.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Drivers = () => {
+  return <div>All Drivers</div>;
+};
+
+export default Drivers;
