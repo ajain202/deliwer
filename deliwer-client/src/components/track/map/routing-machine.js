e033bd2bb692f564ae4c1cb624d38772af768978
aakash jain <92187222+ajain202@users.noreply.github.com>
2022-04-25 23:17:37 -0500

    reactive map component

--- deliwer-client/src/components/track/map/routing-machine.js
+++ deliwer-client/src/components/track/map/routing-machine.js
@@ -2,9 +2,13 @@ import { createControlComponent } from '@react-leaflet/core';
 import L from 'leaflet';
 import 'leaflet-routing-machine';
 
-const createRoutineMachineLayer = (props) => {
-  const instance = L.Routing.control({
-    waypoints: [L.latLng(41.8717, -87.6602), L.latLng(41.8696, -87.6496)],
+const createRoutineMachineLayer = ({currOrder}) => {
+  let lat= currOrder.status==='bid-unaccepted'? 41.8717:currOrder.lat
+  let long=currOrder.status==='bid-unaccepted'? -87.6602:currOrder.long
+  // console.log(lat)
+  // console.log(long)
+  let instance = L.Routing.control({
+    waypoints: [L.latLng(41.8717, -87.6602), L.latLng(lat, long)],
     lineOptions: {
       styles: [{ color: '#6FA1EC', weight: 4 }],
     },
